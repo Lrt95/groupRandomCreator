3586e9a06902220afaed246d08ea25d05ab530a5
Antony Correia <antony.correia@grtgaz.com>
2019-11-07 17:10:10 +0100

    début de projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { GroupComponent } from './group/group.component';
+import { HeaderComponent } from './header/header.component';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    GroupComponent,
+    HeaderComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule
