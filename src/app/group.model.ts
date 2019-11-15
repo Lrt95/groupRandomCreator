58e2fcc28be4fb795b203cae1f04eba65c73a0b1
Antony Correia <antony.correia@grtgaz.com>
2019-11-15 16:41:01 +0100

    reste plus que le style

--- src/app/group.model.ts
+++ src/app/group.model.ts
@@ -2,10 +2,10 @@ import { User } from './user.model';
 
 export class Group {
   public groupName: number;
-  public member: User;
   public imagePath: string;
+  public member: User[];
 
-  constructor(groupName: number, image: string, member: User) {
+  constructor(groupName: number, image: string, member: User[]) {
     this.groupName = groupName;
     this.imagePath = image;
     this.member = member;
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,6 @@
-import {Component, Injectable, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { User } from '../user.model';
-import {DeleteUserGroupComponent} from '../delete-user-group/delete-user-group.component';
-import { Group } from  '../group.model';
+import { Group } from '../group.model';
 
 @Component({
   selector: 'app-users',
@@ -21,10 +20,29 @@ export class UsersComponent implements OnInit {
     new User('Test 8', ''),
     new User('Test 9', ''),
     new User('Test 10', ''),
+    new User('Test 11', ''),
+    new User('Test 12', ''),
+    new User('Test 13', ''),
+    new User('Test 14', ''),
+    new User('Test 15', ''),
+    new User('Test 16', ''),
+    new User('Test 17', ''),
+    new User('Test 18', ''),
+    new User('Test 19', ''),
+    new User('Test 20', ''),
+    new User('Test 21', ''),
+    new User('Test 22', ''),
+    new User('Test 23', ''),
+    new User('Test 24', ''),
+    new User('Test 25', ''),
+    new User('Test 26', ''),
+    new User('Test 27', ''),
   ];
   private delUsers: User[] = [];
-
+  private RandomUsers: User[];
   private groups: Group[] = [];
+  groupeSelect = false;
+
 
   constructor() { }
 
@@ -49,14 +67,44 @@ export class UsersComponent implements OnInit {
     this.users.push(users);
   }
 
-  onGroup(){
+  onGroup() {
     let numberOfGroups: number;
-    console.log(this.users.length);
-    this.users.length % 4 === 0 ? numberOfGroups = this.users.length/4 : numberOfGroups = Math.floor(this.users.length/4);
-    this.getGroups(numberOfGroups)
+    this.users.length % 4 === 0 ? numberOfGroups = this.users.length / 4 : numberOfGroups = Math.floor(this.users.length / 4);
+    this.getGroups(numberOfGroups);
+    this.groupeSelect = true;
     }
 
+  randomEtudiant(array) {
+    let rand;
+    let index = -1;
+    const length = array.length;
+    const result = Array(length);
+    while (++index < length) {
+      rand = Math.floor(Math.random() * (index + 1));
+      result[index] = result[rand];
+      result[rand] = array[index];
+    }
+    return result;
+  }
   getGroups(numberOfGroups: number) {
-    ;
+    this.groups = [];
+    this.RandomUsers = this.randomEtudiant(this.users);
+    let selectGroup = 0;
+    for (let groupName = 1 ; groupName <= numberOfGroups + 1; groupName++) {
+      this.groups.push(new Group(groupName, '', []));
+      this.groups[selectGroup].member = this.RandomUsers.splice(0, 4);
+      selectGroup++;
+    }
+    this.groups[numberOfGroups].member.length  <= 2 ? this.getRegroup(this.groups , numberOfGroups) : this.groups = this.groups;
+    console.log(this.groups);
+  }
+
+  getRegroup(group: Group[], numberOfGroups: number) {
+    let groupIndex = 0;
+    do {
+      groupIndex++;
+      this.groups[numberOfGroups - groupIndex].member.push(this.groups[numberOfGroups].member.pop());
+    } while (this.groups[numberOfGroups].member.length !== 0);
+    this.groups.pop();
   }
 }
