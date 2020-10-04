fa74d2bf9810b0cfe6a396ed03aa455c15fa22f9
Antony Correia <antony.correia@grtgaz.com>
2020-10-05 00:47:28 +0200

    make public auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ import {AuthService} from './auth/auth.service';
 })
 export class AppComponent {
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService) { }
   title = 'groupRandomCreator';
 }
